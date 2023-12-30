22f22439d7477ce2f1712e13b09d22e93d4a40ba
Vishesh813 <vishesh.tiwari813@gmail.com>
2023-12-30 22:05:25 +0530

    updated repo

--- src/components/router.js
+++ src/components/router.js
@@ -5,15 +5,15 @@ import About from './about';
 import Project from './project';
 import Resume from './resume'; 
 import Contact from './contact';
-import { Switch,Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 const Routers = ()=> (
-     <Switch>
+     <Routes>
       <Route exact path="/" component={Mainpage} />
       <Route  path="/error" component={About} />
       <Route  path="/project" component={Project} />
       <Route  path="/resume" component={Resume} />
       <Route  path="/contact" component={Contact} />
-     </Switch>
+     </Routes>
 )
 
 export default Routers;
