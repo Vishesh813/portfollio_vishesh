7f52011ac3fe1bdbc82becfdaf27fa6945b5fdf9
Vishesh Tiwari <42551757+Vishesh813@users.noreply.github.com>
2024-03-03 15:14:15 +0530

    added github link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
@@ -11,7 +11,4 @@ ReactDOM.render(
 <HashRouter><App /></HashRouter>,
  document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
